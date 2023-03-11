1453d11986d31db0abda091e4ba0b9b1d36fe218
kiko7792 <josebarreirinho92@gmail.com>
2023-03-11 13:38:08 +0000

    Initial commit

---
+++ src/main/java/org/teamdai/sivoleivsc/SiVoleiVscApplication.java
@@ -0,0 +1,13 @@
+package org.teamdai.sivoleivsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiVoleiVscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiVoleiVscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/teamdai/sivoleivsc/SiVoleiVscApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamdai.sivoleivsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiVoleiVscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
